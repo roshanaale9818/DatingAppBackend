3c9d38a763ec7e76938bea82dd1110d8454f930c
roshanaale9818 <itachiuchhia9818@gmail.com>
2021-02-11 09:24:23 +0545

    first commit

---
+++ DatingAppBackend/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using DatingAppBackend.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingAppBackend.Data
+{
+    //we should derive from db context of entity framework
+    public class DataContext:DbContext
+    {
+        //for database connections
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        //generating DBset for Users
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
